5f107f9840da14c8e5ee7ba6e7d48736f9785b11
flat35hd99 <flat35hd99@gmail.com>
2022-10-27 01:16:31 +0900

    Use http instead of tcp

--- server.go
+++ server.go
@@ -1,47 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"log"
-	"net"
-	"time"
+	"net/http"
 )
 
 func main() {
-	// tcpの接続アドレスを作成する
-	tcpAddr, err := net.ResolveTCPAddr("tcp", ":3030")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// リスナーを作成する
-	listener, err := net.ListenTCP("tcp", tcpAddr)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Start TCP Server...")
-	for {
-		// クライアントからのコネクション情報を受け取る
-		conn, err := listener.AcceptTCP()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		res, err := io.ReadAll(r.Body)
 		if err != nil {
 			log.Fatal(err)
 		}
+		w.Write(res)
+	})
 
-		// ハンドラーに接続情報を渡す
-		echoHandler(conn)
-	}
-}
-
-func echoHandler(conn *net.TCPConn) {
-	defer conn.Close()
-	for {
-		// リクエストを受け付けたらサーバー側に「response from server」を返す
-		_, err := io.WriteString(conn, "response from server\n")
-		if err != nil {
-			return
-		}
-		time.Sleep(time.Second)
-	}
+	http.ListenAndServe(":8080", nil)
 }
